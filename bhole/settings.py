40d0beb09f7d20b1efda69422390556902038aac
ankii9600 <ishendra9897@gmail.com>
2019-06-10 13:49:27 -0700

    modified settings

--- bhole/settings.py
+++ bhole/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '85!yr*8wpi%2q+ttaj%4q7c+h^0-fh4uk7!ykm#4#)*=iguv!z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
