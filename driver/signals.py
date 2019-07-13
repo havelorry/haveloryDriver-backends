2f706f4c4466f50aa314c358ad08d3bd745c4d76
ankii9600 <ar31213@gmail.com>
2019-07-13 21:08:28 +0530

    notifications added

--- driver/signals.py
+++ driver/signals.py
@@ -1,10 +1,11 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.forms import model_to_dict
-from driver.models import activeLogin
+from driver.models import activeLogin, Ride,Notification
 from channels.layers import get_channel_layer
 import json
 from asgiref.sync import async_to_sync
+from driver.views import send_push_message
 
 @receiver(post_save, sender=activeLogin)
 def announce_user(instance,**kwargs):
@@ -19,3 +20,21 @@ def announce_user(instance,**kwargs):
         'event':"updatedDrivers",
         'available':c 
     })
+
+
+@receiver(post_save,sender=Ride)
+def announce_rider(instance,**kwargs):
+    userId = instance.username_id
+    driver_id = instance.driver_id
+
+    userTResult = Notification.objects.get(username=userId)
+    driverTResult = Notification.objects.get(username=driver_id)
+
+    if userTResult is not None:
+        send_push_message(userTResult.token,"{} ride is {}".format(userId,instance.status))
+
+    if driverTResult is not None:
+        send_push_message(driverTResult.token,"{} a ride has been assigned to You see in details".format(instance.driver_id))
+
+
+
